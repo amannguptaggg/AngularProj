00c1986ac5d7aa76808f217d22de89da9ee09e2e
amannguptaggg <amannguptaggg@gmail.com>
2019-07-12 19:07:35 +0530

    use of grouping in *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,66 +8,38 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My New Project';
 Student : any[];
- constructor() {
-   this.Student = [
-     {
-       stdentId:1,
-       name:'Aman',
-       gender:'Male',
-       age: 20,
-       Course:'MCA'
-     },
-     {
-      stdentId:2,
-      name:'Ajit',
-      gender:'Male',
-      age: 21,
-      Course:'BCA'
-    },
-    {
-      stdentId:3,
-      name:'Swati',
-      gender:'Female',
-      age: 18,
-      Course:'B.sc'
-    }
-   ];
- }
-getMore():void {
-  this.Student = [
-    {
-      stdentId:1,
-      name:'Aman',
-      gender:'Male',
-      age: 20,
-      Course:'MCA'
-    },
-    {
-     stdentId:2,
-     name:'Ajit',
-     gender:'Male',
-     age: 21,
-     Course:'BCA'
-   },
-   {
-     stdentId:3,
-     name:'Swati',
-     gender:'Female',
-     age: 18,
-     Course:'B.sc'
-   },
-   {
-    stdentId:4,
-    name:'Ranuj',
-    gender:'Male',
-    age: 20,
-    Course:'B.pharma'
+ constructor() { 
+}
+CountryDetails:any[] =[
+  {
+    'country': 'India',
+    'people' : [
+      {
+      "name" : "ajeet kumar",
+      },
+      {
+        "name" : "Aman Gupta",
+      }
+        ,
+      {
+          "name" :"Rahul Singh",
+      }
+    ],
+  },
+  {
+    'country': 'UK',
+    'people' : [
+      {
+      "name" : "ABC",
+      },
+      {
+        "name" : "EFG",
+      }
+        ,
+      {
+          "name" :"HIJ",
+      }
+    ],
   }
-  ]
-}
-
-trackBystdentId(index:number,student:any):string{
- return student.stdentId;
-}
-
+]
 }
