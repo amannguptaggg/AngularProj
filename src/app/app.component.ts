ce6940cd177d66f170cae77890b7458b00f592a0
amannguptaggg <amannguptaggg@gmail.com>
2019-07-12 15:49:46 +0530

    Attribute Directive Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My New Project';
+  checkIf = false;
   btnClickMe() {
     alert('you click Click me btn');
   }
