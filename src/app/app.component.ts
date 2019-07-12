6fc5881169a64030de68a5d8ed0534e3e6dfb0e7
amannguptaggg <amannguptaggg@gmail.com>
2019-07-12 18:51:09 +0530

    use of trackBy with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My New Project';
-Student : any[] = [
-  {
-    'name':'Aman'
-  },
-  {
-    'name' :'Rahul'
-  },
-  {
-    'name' : 'Mahesh'
-  },
-  {
-    'name': 'Golu'
+Student : any[];
+ constructor() {
+   this.Student = [
+     {
+       stdentId:1,
+       name:'Aman',
+       gender:'Male',
+       age: 20,
+       Course:'MCA'
+     },
+     {
+      stdentId:2,
+      name:'Ajit',
+      gender:'Male',
+      age: 21,
+      Course:'BCA'
+    },
+    {
+      stdentId:3,
+      name:'Swati',
+      gender:'Female',
+      age: 18,
+      Course:'B.sc'
+    }
+   ];
+ }
+getMore():void {
+  this.Student = [
+    {
+      stdentId:1,
+      name:'Aman',
+      gender:'Male',
+      age: 20,
+      Course:'MCA'
+    },
+    {
+     stdentId:2,
+     name:'Ajit',
+     gender:'Male',
+     age: 21,
+     Course:'BCA'
+   },
+   {
+     stdentId:3,
+     name:'Swati',
+     gender:'Female',
+     age: 18,
+     Course:'B.sc'
+   },
+   {
+    stdentId:4,
+    name:'Ranuj',
+    gender:'Male',
+    age: 20,
+    Course:'B.pharma'
   }
-]
+  ]
+}
 
+trackBystdentId(index:number,student:any):string{
+ return student.stdentId;
+}
 
 }
