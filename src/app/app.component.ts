e35d36f23df0ccba7d78ee2808fbf120821b34cc
amannguptaggg <amannguptaggg@gmail.com>
2019-07-13 18:28:02 +0530

    Using a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {NewServiceService} from './new-service.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NewServiceService]
 })
 export class AppComponent {
   title = 'My New Project';
+  text: string;
 
- 
+ constructor(private _newService:NewServiceService) {}
+ ngOnInit() {
+   this.text = this._newService.display();
+ }
 }
