0fe39b75e333c9dec34ca2a9f9030c15224e8b15
amannguptaggg <amannguptaggg@gmail.com>
2019-07-16 23:49:59 +0530

    wasteFile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {FormGroup,FormControl,FormBuilder,Validators,FormArray, NgForm} from '@angular/forms';
-import {User} from "./user"
 import { format } from 'path';
 @Component({
   selector: 'app-root',
@@ -9,31 +7,5 @@ import { format } from 'path';
 })
 export class AppComponent {
   title = 'My New Project';
-   user = new User();
-
-   constructor() {}
-
-   onFormSubmit(form:NgForm) {
-     if(form.invalid){
-       return;
-     }
-     console.log('User Name:'+form.controls['uName'].value);
-     console.log('Gender Name:'+form.controls['gender'].value);
-     console.log('User Married:'+form.controls['married'].value);
-     console.log('User T & C:'+form.controls['tc'].value);
-   }
-
-   resetForm(form:NgForm) {
-     this.user= new User();
-     form.resetForm({
-        married:false
-   });
-   }
-
-   setDefault() {
-     this.user.userName="Name";
-     this.user.gender="Gender";
-     this.user.isMarried=false;
-     this.user.isTCAccepted=true;
-   }
+   
 }
