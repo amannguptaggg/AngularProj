5cd999d295d5f5fc838119cb5f294866211df1ba
amannguptaggg <amannguptaggg@gmail.com>
2019-07-21 14:18:26 +0530

    Waste Code For Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { ActivateGuard } from './activate.guard';
-import { UserService } from './user.service';
-import { HomeComponent } from './home/home.component';
-import {AppRoutes} from './Routing';
-import { MerchantComponent } from './merchant/merchant.component';
-import { DeactivateGuard } from './deactivate.guard';
-import { CustomerComponent } from './customer/customer.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
-import { ActivateChildGuard } from './activate-child.guard';
-import { ViewDetailsComponent } from './view-details/view-details.component';
-import { BookServiceService } from './book-service.service';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
-    HomeComponent,
-    MerchantComponent,
-    CustomerComponent,
-    AddCustomerComponent,
-    ViewDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ActivateGuard,UserService,DeactivateGuard,ActivateChildGuard,BookServiceService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
