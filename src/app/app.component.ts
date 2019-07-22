354bc5617ce188997f6d91aec44c19b90ff8787c
amannguptaggg <amannguptaggg@gmail.com>
2019-07-22 13:31:14 +0530

    using ng-template (toggle)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project Routing';
-
+  value=false;
 constructor() {}
-
 ngOnInit() { 
 }
 
+display() {
+
+   if(this.value==false) {
+
+     this.value = true;
+
+   } else{
+     this.value = false;
+
+    }
+    console.log(this.value);
+}
 
 }
