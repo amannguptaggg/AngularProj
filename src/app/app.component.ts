6d082006d9472fd28ef5a8a2655fba20b6184f59
amannguptaggg <amannguptaggg@gmail.com>
2019-07-23 09:36:46 +0530

    Using HostLister to manupulate events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,HostListener,Renderer, ElementRef} from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -10,8 +10,25 @@ export class AppComponent {
   title = 'Project Routing';
 
 
-constructor() {}
+constructor(private el:ElementRef,private render:Renderer) {}
 ngOnInit() { 
  
 }
+
+@HostListener('click') onclick() {
+  alert('Clicked');
+}
+
+@HostListener('mouseover') mouseover() {
+  this.ChnagebgColor('red');
+}
+@HostListener('mouseleave') onmouseleave() {
+  this.ChnagebgColor('blue');
+}
+
+ChnagebgColor(color:string) {
+  this.render.setElementStyle(this.el.nativeElement,'color',color);
+}
+
+
 }
