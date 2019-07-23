c6b08997e760b6f36975a594973f09a96f43fd16
amannguptaggg <amannguptaggg@gmail.com>
2019-07-23 08:51:57 +0530

    Using ViewChild Using Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { ParentCounterComponent } from './parent-counter/parent-counter.component';
+import { ChangeColorDirective } from './change-color.directive';
+import { ParentColorComponent } from './parent-color/parent-color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     ParentCounterComponent,
+    ChangeColorDirective,
+    ParentColorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/parent-color/parent-color.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeColorDirective } from '../change-color.directive';
+
+@Component({
+  selector: 'app-parent-color',
+  templateUrl: './parent-color.component.html',
+  styleUrls: ['./parent-color.component.css']
+})
+export class ParentColorComponent implements OnInit {
+
+@ViewChild(ChangeColorDirective,{static:false}) chnagecolordirective:ChangeColorDirective;
+
+  constructor() { }
+
+chnageColor(color:string){
+  this.chnagecolordirective.change(color);
+}
+
+  ngOnInit() {
+  }
+
+}
